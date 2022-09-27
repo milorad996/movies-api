3703b63c21f6ac05cfce4214bdc76cd14cfc9808
milorad996 <mips96.1@gmail.com>
2022-09-27 12:44:23 +0200

    #CSDT-58

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return response()->json([
+            'token' => $token,
+            'user' => Auth::user(),
+        ]);
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->validated();
+        $data['password'] = Hash::make($data['password']);
+        $user = User::create($data);
+
+        $token = Auth::login($user);
+
+        return response()->json([
+            'token' => $token,
+            'user' => Auth::user(),
+        ]);
+    }
+
+    public function getMyProfile()
+    {
+
+        $activeUser = Auth::user();
+        return response()->json($activeUser);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response(null, 204);
+    }
+
+    public function refreshToken()
+    {
+        $token = Auth::refresh();
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+}
