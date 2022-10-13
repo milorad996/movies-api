17651fb3e118793a850635cd16c28543a31e55d9
milorad996 <mips96.1@gmail.com>
2022-10-13 13:08:17 +0200

    #CSDT-80

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <body>
     <h1>A new movie is added to the system</h1>
     <h3>{{$email_data->title}}</h3><br>
-    <h3>{{$email_data->description}}</h3><br>
+    <h3>{{$email_data->description}}</h3><br><br>
     <img src="{{ $email_data->image }}" >
 </body>
 </html>
